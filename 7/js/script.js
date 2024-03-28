a4cbc18d20fa1929a41c2d9442fa2efa5b2719f5
Keks <keks@htmlacademy.ru>
2024-03-28 11:53:49 +0300

    :heavy_check_mark: Сборка #7

---
+++ 7/js/script.js
@@ -0,0 +1,14 @@
+// Для проверки работы скриптов в браузере
+if (document.querySelector('.header-nav--nojs')) {
+  document.querySelector('.header-nav--nojs').classList.remove('header-nav--nojs');
+}
+
+
+// Меню
+const menu = document.querySelector('.header-nav');
+const menuBtn = document.querySelector('.header-nav__toggle');
+
+menuBtn.addEventListener('click', (evt) => {
+  evt.preventDefault();
+  menu.classList.toggle('header-nav--open');
+});
